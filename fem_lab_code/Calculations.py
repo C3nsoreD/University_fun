4b4901a6922214d52230fc087be17039ba23d2c7
c3nsored <ur.gabriel@outlook.com>
2021-01-26 16:04:40 +0100

    edits to soln

--- fem_lab_code/Calculations.py
+++ fem_lab_code/Calculations.py
@@ -10,38 +10,32 @@ IXX = 100970
 IYY = 355416
 A = 650 
 E = 200000
-DEN_ = 7.810e-9
+DEN_ = 7.8e-9
 L = 1140  
 
-case1 = lambda i: 0.393 * math.pow((2*i - 1), 2)
-case2 = lambda i: 0.098 * math.pow((4*i + 1), 2)
-case3 = lambda i: 0.393 * math.pow((2*i + 1), 2)
-case4 = lambda i: 0.393 * math.pow((2*i - 1), 2)
+case1 = lambda i: 0.393 * ((2*i) - 1)**2
+case2 = lambda i: 0.393 * ((2*i) + 1)**2
+case3 = lambda i: 0.098 * ((4*i) + 1)**2
+case4 = lambda i: 0.393 * ((2*i) - 1)**2
 
 equations = {
-    'case1': case1,
-    'case2': case2,
-    'case3': case3,
-    'case4': case4,
+    'case1': [0.560, 3.506, 9.825],
+    'case2': [2.454, 7.951, 16.562],
+    'case3': [3.560, 9.815, 19.257],
+    'case4': [1, 1, 1],
 }
 
 numerial_results = {
-    'case1': [27.179, 43.101, 101.98, 168.53, 192.62, 385.91, 463.72, 480.29],
-    'case2': [49.696, 118.27, 152.36, 323.22, 377.10, 406.98, 617.58, 767.08],
+    'case2': [27.179, 43.101, 101.98, 168.53, 192.62, 385.91, 463.72, 480.29],
+    'case1': [49.696, 118.27, 152.36, 323.22, 377.10, 406.98, 617.58, 767.08],
     'case3': [150.92, 170.69, 358.37, 446.64, 460.61, 637.67, 876.03, 979.77],
     'case4': [180.07, 201.77, 413.37, 458.64, 506.73, 707.13, 928.28, 1059.1],
 }
 
 
-def c_analytic(moi):
-    """ 
-        Analytic equation thats dependant on moment of inertia
-    
-    """
-    _term1 = math.sqrt((E*moi)/(DEN_*A))
-    _term2 = 1 / L**2
-    return (_term2 * _term1)
-
+_term1 = 1 / L**2
+_term2 = math.sqrt((E*IYY)/(DEN_*A))
+c = _term1 * _term2
 
 def _Fi_c(moi):
     """
@@ -49,24 +43,28 @@ def _Fi_c(moi):
         the bending mode 
     """
 
-    results = {}
-    f_values = {}
-    for c, f in equations.items():
-        # results[c] = [f(i) for i in range(1, 9)]
-        _fi = []
-        f_c = []
-        for i in range(1, 9):
-            if i in [1, 4, 7]:
-                _fi.append(f(i) * c_analytic(moi[0]))
-                f_c.append(f(i))
-            else:
-                _fi.append(f(i) * c_analytic(moi[1]))
-                f_c.append(f(i))
+    F_C = {}
+    F = {}
+    for case, _f in equations.items():
+        # f_c = [] 
+        f = []
+        f = [i * c for i in _f]
+        
+        # for i in range(1, 4):
+        #     if i < 3:
+        #         f_c.append(_f[i])
+        
+        # print("length of F_c \n", (f_c))
 
-        results[c], f_values[c] = f_c, _fi
+        # f =  [c * f_c[i] for i in range(len(f_c)) if i < 2]
+        # _v_3 = f_c[2](3) * c
+
+        # f =  [f_c[2](3) ]
+        # print("f \n", _v_3)
+        F_C[case], F[case] = _f, f
         # f_values[c] = _values
 
-    return results, f_values
+    return F_C, F
 
 
 def relative_error(analytic_res, n_results):
@@ -79,7 +77,7 @@ def relative_error(analytic_res, n_results):
     return _error
 
 
-def write_csv(results, filename="results.csv"):
+def write_csv(results, filename=None):
     field_names = [name for name in results.keys()]
     values = [v for v in results.values()]
     # print(values, end='\n')
@@ -94,22 +92,28 @@ def write_csv(results, filename="results.csv"):
 if __name__ == "__main__":
     moi = [IXX, IYY]
     
-    results_xx, f_xx = _Fi_c(moi)
-
+    f_C_xx, f_xx = _Fi_c(moi)
     results_yy, f_yy = _Fi_c(moi)
     
     _error_xx = relative_error(f_xx, numerial_results)
     _error_yy = relative_error(f_yy, numerial_results)
     
-    write_csv(results_xx, "results_xx.csv")
-    write_csv(results_yy, "results_yy.csv")
+    print("F results in xx\n", f_xx)
+    print()
+    print("F/C xx \n", f_C_xx)
+    print("error in yy \n",_error_xx)
+    # print("error in xx, \n",_error_yy)
+    
+    # write_csv(results_xx, "results_xx.csv")
+    # write_csv(results_yy, "results_yy.csv")
 
     write_csv(_error_xx, "error_xx.csv")
-    write_csv(_error_yy, "error_yy.csv")
+    # write_csv(_error_yy, "error_yy.csv")
     write_csv(f_xx, "f_xx.csv")
-    write_csv(f_yy, "f_yy.csv")
+    # write_csv(f_yy, "f_yy.csv")
     
+    print()
     # Tests 
-    print(c_analytic(moi[0]))
+    print(c)
 
     
