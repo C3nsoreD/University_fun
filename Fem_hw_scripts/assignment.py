bebf9f7091af897f503c942a68fabc479c506f30
c3nsored <u.gabrielr@gmail.com>
2021-01-29 17:02:27 +0100

    clean up

--- Fem_hw_scripts/assignment.py
+++ Fem_hw_scripts/assignment.py
@@ -28,7 +28,7 @@ def T(c, s):
 	return value
 	
 beta = -np.degrees((np.arctan(height/length)))
-beta = 180 + beta
+# beta = 180 + beta
 # print(beta)
 
 # Element 1 values 
@@ -70,28 +70,29 @@ _global = [
 
 # component of global matrix 
 # calculates for u3 and v3
-_x = [[K_1[2, 2]+K_2[2,2], K_2[2, 3]+K_1[2, 3]],
+__x = [[K_1[2, 2]+K_2[2,2], K_2[2, 3]+K_1[2, 3]],
 	  [K_1[3, 2]+K_2[3,2], K_2[3, 3]+K_1[3, 3]],]
+_x = _global[2:][2:]
 
-
+# Force constants for elements
 Ft1 = alpha1*deltaT*E1*A1
 Ft2 = alpha2*deltaT*E2*A2
 
-
+# the components of element 2
 ft2x = Ft2*(_c(beta))
 ft2y = Ft2*(_s(beta))
 
-# Forces = [[Ft1 + ft2x], [-ft2y]]
+# Individual force vectors for each beam element 
+Forces_1 = [[-Ft1], [0], [Ft1], [0]]
+Forces_2 = [[-ft2x], [ft2y], [ft2x], [-ft2y]]
 
-Forces_1 = [[Ft1], [0], [-Ft1], [0]]
-Forces_2 = [[ft2x], [ft2y], [-ft2x], [-ft2y]]
-
-total_f = [Forces_1[0], Forces_1[1], Forces_2[0], Forces_2[1], [Forces_1[2][0]+Forces_2[2][0]], [Forces_1[3][0]+Forces_2[3][0]]]
+# Total force vectors
+total_f = [Forces_1[0], Forces_1[1], Forces_2[0], Forces_2[1], [Forces_1[2][0]+Forces_2[2][0]], Forces_2[3]]
 # m = 0.0000000000000000001
 # inv_global = np.linalg.inv(_global + np.eye(np.asmatrix(_global).shape[1])*m) 
 
-inv_x = np.linalg.inv(_x) 
-# print(total_f)
+inv_x = np.linalg.inv(__x)
+
 node_3 = inv_x.dot(np.asmatrix(total_f[2:][2:]))
 
 q_total = [[0], [0], [0], [0], [node_3[0]], [node_3[1]]]
@@ -99,62 +100,81 @@ q_total = [[0], [0], [0], [0], [node_3[0]], [node_3[1]]]
 
 zz = np.matmul(_global, q_total)
 
-r1 = zz[0] - total_f[0]
-r1 = zz[0] - total_f[0]
-r2 = zz[1] - total_f[1]
-r3 = zz[2] - total_f[2]
-r4 = zz[3] - total_f[3]
-r6 = zz[5] - total_f[5]
+### Stress calculations
+"""
+Calculation of stress components
+"""
+# element 1
+_e1 = node_3[0]/length - alpha1*deltaT
+sigma1 = _e1 * E1
+N1 = sigma1 * A1
+# element 2
+_e2 = (node_3[0]*c2 + node_3[1]*s2)/length2 - alpha2*deltaT
+sigma2 = _e2 * E2
+N2 = sigma2 * A2
 
-# print(zz)
-
-# Problem 2
+# reactions
+reactions = [zz[i] - total_f[i] for i in range(len(total_f))]
 
 _x_2 = K_1[2, 2] + K_2[2, 2]
 
-q5 = total_f[2]/_x_2
+q5 = total_f[4]/_x_2
 
-_q_total = [[0], [0], [0], [0], q5, [0]]
+_q_total = [[0], [0], [0], [0], [q5], [0]]
 _zz = np.matmul(_global, _q_total)
 
 # print(np.asmatrix(_global).round(4))
-print(f"_zz {np.asmatrix(_zz).round(2)}")
+# print(f"_zz {np.asmatrix(_zz).round(2)}")
 
-_r1 = _zz[0] - total_f[0]
-_r2 = _zz[1] - total_f[1]
-_r3 = _zz[2] - total_f[2]
-_r4 = _zz[3] - total_f[3]
-_r6 = _zz[5] - total_f[5]
+reactions_2 = [(_zz[i] - total_f[i]) for i in range(len(total_f))]
 
-# print(_r1)
-print(q5)
-print(_r1)
-print(_r2)
-print(_r3)
-print(_r4)
-print(_r6)
+## print(_r1)
+
+### Stress calculations
+_e1 = q5/length - alpha1*deltaT
+sigma1 = _e1 * E1
+N1 = sigma1 * A1
+# element 2
+_e2 = (-q5*c2)/length2 - alpha2*deltaT
+sigma2 = _e2 * E2
+N2 = sigma2 * A2
+
+print("####### Problem 2")
+print(f"displacement for p2: {q5}")
+
+print("REACTIONS for 2")
+print([round(float(i), 3) for i in np.asmatrix(reactions_2)])
+
+print(
+	"The resulting stress for part a: \n"
+	f"e1:{_e1} sigma 1{sigma1} N1:{N1}"
+)
+print(
+	"The resulting stress for part a: \n"
+	f"e2:{_e2} sigma 2 {sigma2} N2:{N2}"
+)
+print()
+
+
+print("#### Problem 1")
+print("Global stiffness matrix")
+print(np.asmatrix(__x).round(2))
+print()
+print("REACTIONS")
+print([round(float(i), 3) for i in np.asmatrix(reactions)])
+print()
+print("Total Force matrix") 
+# print(# np.asmatrix(inv_x))
+print(np.asmatrix(total_f))
 
 print()
-print(r1)
-print(r2)
-print(r3)
-print(r4)
-print(r6)
-# Prints -----------------------------
-# print(np.asmatrix(_global).round(3))
-# print(s1, s2, c1, c2)
-# print(inv_global.dot(np.asmatrix(Forces)))
-print()
-print(f"Results of displacement {node_3} \n")
-# print((q_total))
-# print(np.asmatrix(total_f))
-# print("individual nodes")
-# print(node_3_1)
-# print(np.asmatrix(Forces).dot(inv_global))
-# print(np.asmatrix(K_1))
-# print(np.degrees(beta))
-# print(length2)
-# print(f'Ft1: {Ft1} ft2y: {ft2y} ft2x: {ft2x} Ft2 {Ft2}')
-# print(np.asmatrix(K_2))
+print(f"Displacements {[float(i) for i in node_3.round(3)]}")
 
-# print(total_f[2:][2:])
+# print(
+# 	"The resulting stress for part a: \n"
+# 	f"e2:{_e2} sigma 2 {sigma2} N2:{N2}"
+# )
+# print(
+# 	"The resulting stress for part a: \n"
+# 	f"e1:{_e1} sigma 1{sigma1} N1:{N1}"
+# )
